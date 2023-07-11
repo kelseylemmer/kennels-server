88fb0a5a488b07b4fbedba0297904bdeb50cdcaf
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-07-11 16:53:05 -0400

    do_GET for animals, employees, customers, locations

---
+++ views/customer_requests.py
@@ -0,0 +1,23 @@
+CUSTOMERS = [
+    {
+        "id": 1,
+        "name": "Ryan Tanay"
+    }
+]
+
+def get_all_customers():
+    return CUSTOMERS
+
+# Function with a single parameter
+def get_single_customer(id):
+    # Variable to hold the found customer, if it exists
+    requested_customer = None
+
+    # Iterate the EMPLOYEES list above.
+    for customer in CUSTOMERS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
