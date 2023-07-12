b2980f2f6e7d3b6fed7e4aa7bf5fc1c342f82a18
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-07-12 16:04:36 -0400

    updated customers and employees to match client side

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,7 +1,8 @@
 CUSTOMERS = [
     {
         "id": 1,
-        "name": "Ryan Tanay"
+        "fullName": "Ryan Tanay",
+        "email": "rtanay@gmai.com"
     }
 ]
 
@@ -21,3 +22,19 @@ def get_single_customer(id):
             requested_customer = customer
 
     return requested_customer
+
+def create_customer(customer):
+    # Get the id value of the last animal in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    customer["id"] = new_id
+
+    # Add the animal dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
