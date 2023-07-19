acdf5d4b8b61041143ad6d8bdb00ce3e1d12d6c4
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-07-19 10:37:41 -0400

    fixes

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Animal
+
 ANIMALS = [
     {
         "id": 1,
@@ -104,3 +108,47 @@ def update_animal(id, new_animal):
             # Found the animal. Update the value.
             ANIMALS[index] = new_animal
             break
+
+def get_all_animals():
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.species,
+            a.breed,
+            a.status,
+            a.locationId,
+            a.customerId,
+            a.employeeId,
+            a.status
+        FROM animal a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        animals = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+
+            animals.append(animal.__dict__)
+
+    return animals
